14be7b0709476fa46f5c810f2da0365d663c7e6a
yang <2819326527@qq.com>
2022-01-16 17:47:39 +0800

    更新tools：lhs没有生成target.png文件，可以由tools文件夹中的draw_generations.py生成

---
+++ tools/README.md
@@ -0,0 +1,10 @@
+# tools
+lab project for graduation reply
+
+辅助代码
+
+<b> draw_generations.py </b>: 有时候bo没有自动生成ineration过程中的target曲线图，借用该代码生成曲线图放入config文件夹中。
+
+输入：bo的logs.json文件
+
+输出：x轴为interations，y轴为target曲线图
---
+++ tools/draw_generations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：tools 
+@File ：draw_generations.py
+@Author ：Yang
+@Date ：2022/1/16 17:12 
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+import  json
+def draw_target(x, y, init_points, n_iter):
+    # 画图
+    plt.plot(x, -y, label='lhs_bo  init_points = ' + str(init_points) + ', n_iter = ' + str(n_iter))
+    max = y.max()
+    max_indx = y.argmax()
+    # 在图上描出执行时间最低点
+    plt.scatter(max_indx, -max, s=20, color='r')
+    plt.xlabel('iterations')
+    plt.ylabel('runtime')
+    plt.legend()
+    plt.savefig("./lhs_target.png")
+    plt.show()
+
+if __name__ == '__main__':
+    logpath = './logs.json'
+    init_points = 15
+    n_iter = 30
+
+    target = []
+    interations = []
+    inters = 0
+    for line in open(logpath, 'r'):
+        data = json.loads(line)
+        target.append(data['target'])
+        interations.append(inters)
+        inters += 1
+    x = np.array(interations)
+    y = np.array(target)
+    draw_target(x, y, init_points, n_iter)
